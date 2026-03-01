9035364b0ed2203e94ec3c568718c22b5a577b1e
bagons <104223895+bagons@users.noreply.github.com>
2026-03-02 00:11:17 +0100

    update normal map demo

--- normal_map.cpp
+++ normal_map.cpp
@@ -4,39 +4,50 @@
 
 Engine ge{"normal maps test", 500, 500};
 
+enum Actions {
+    SWITCH
+};
+
 int main() {
+    ge.input.set_action_list({
+        GLFW_KEY_SPACE
+    });
+
     ge.lights.ambient_light = Color::WHITE;
 
     auto camera = ge.add<Camera>(45.0f, 0.1f, 100.0f);
     camera->transform.position = Position{0.0f, 3.0f, 10.0f};
     camera->transform.rotation = Rotation{Engine::PI / 12, 0, 0.0f};
-    auto opaque_pass = ge.add_render_layer<ForwardOpaque3DPass>(camera);
+    auto opaque_pass = ge.add_render_pass<ForwardOpaque3DPass>(camera);
+    auto bg = ge.add_render_pass<ColorPass>(Color::BLACK);
 
     auto cube_model = std::make_shared<Model>("res_demo2/box.obj");
     auto cube = ge.add<ModelThing>(cube_model);
     cube->transform.scale = Scale{2.0f};
 
-    auto cube_mesh = std::make_shared<Mesh>("res_demo2/box.obj", true);
 
     auto point_light = ge.add<PointLight>(Color::WHITE, 30.0f);
     point_light->transform.position.z = 4.0f;
-    auto light_indicator = ge.add<MeshThing>(cube_mesh, ge.shaders.get_base_material(true, true, true));
-    light_indicator->transform.scale = Scale{0.1f};
-    light_indicator->transform.position.z = point_light->transform.position.z;
+
+    bool bump_map_switch = true;
 
     while (ge.is_running()) {
-        cube->transform.rotation.y = static_cast<float>(ge.get_game_time() * 0.1);
+        ge.pool_inputs();
+        if (ge.input.just_pressed(SWITCH)) {
+            std::cout << "pressed SWITCH" << std::endl;
+            bump_map_switch = !bump_map_switch;
+            //mat->set_uniform("material.bump_map_strength", bump_map_switch ? 6.0f : 0.0f);
+        }
 
         ge.update();
 
+        cube->transform.rotation.y = static_cast<float>(ge.get_game_time() * 0.1);
+
         // render
-
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        //glClearColor(0.0745f, 0.659f, 1.0f, 0.353f);
-
+        bg->render();
         opaque_pass->render();
 
-        ge.send_it_to_window();
+        ge.send_to_window();
     }
 
     return 0;
